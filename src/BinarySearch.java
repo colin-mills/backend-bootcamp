3476dc0a32ba8287333b149f2e6b1a44613093cc
Colin Mills <cgm71@georgetown.edu>
2022-04-09 18:59:53 -0500

    Generic Binary Search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,14 +1,45 @@
-int[] newArr = new int[(sortedNums.length + 1)];
-        int j = 0;
+public class BinarySearch<T extends Comparable<T>> {
+    T[] sortedArr;
 
-        for (int i = 0; i < newArr.length; i++) {
+    public static void main(String[] args) {
+        //code to test binary search method
 
-        if (i == index) {
-        newArr[i] = value;
-        } else {
-        newArr[i] = sortedNums[j];
-        j++;
-        }
-        }//place each value, shifting over when we are at our new index
+        BinarySearch<Character> ob = new BinarySearch<>(new Character[]{'a', 'b', 'd', 'e', 'f', 'g'});
+        System.out.println(ob.Search('a'));
+        System.out.println(ob.Search('b'));
+        System.out.println(ob.Search('c'));
+        System.out.println(ob.Search('d'));
+        System.out.println(ob.Search('e'));
+        System.out.println(ob.Search('z'));
+        System.out.println(ob.Search('A'));
 
-        this.sortedNums = newArr;
+    }
+
+    public BinarySearch (T[] arr) {
+        //Assumes that it will be passed a sorted array of type T
+        this.sortedArr = arr;
+    }
+
+    public int Search(T val) {
+        int l = 0, h = sortedArr.length -1, m =0;
+        boolean found = false;
+
+        while (!found && l <= h) {
+            m = l + (h - l) / 2;
+
+            if (sortedArr[m] == val) {
+                found = true;
+            } else if (val.compareTo(sortedArr[m]) < 0 ) {
+                //Search bottom half
+                h = m - 1;
+            } else {
+                //search top half
+                l = m + 1;
+            }
+        }//While searching
+        if (!found)
+            m = -1;
+
+        return m;
+    }
+}
